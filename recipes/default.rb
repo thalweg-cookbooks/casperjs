6b872cfe69beb525d3e7ddb8c17402608ad52e7b
Nick Lopez <nlopez@amplify.com>
2015-06-22 18:42:11 -0400

    mvp

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,4 +2,13 @@
 # Cookbook Name:: casperjs
 # Recipe:: default
 #
-# Copyright (c) 2015 The Authors, All Rights Reserved.
+# Copyright (c) 2015 Nick Lopez, All Rights Reserved.
+
+include_recipe 'phantomjs'
+
+ark 'casperjs' do
+  url 'https://github.com/n1k0/casperjs/archive/1.1-beta3.tar.gz'
+  creates 'casperjs'
+  path '/usr/local/bin'
+  action :cherry_pick
+end
