9db46a3ea554498d589b1ee3b2bbf487c222493b
Nick Lopez <nlopez@amplify.com>
2015-06-22 18:27:31 -0400

    init

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'casperjs'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures casperjs'
+long_description 'Installs/Configures casperjs'
+version '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: casperjs
+# Recipe:: default
+#
+# Copyright (c) 2015 The Authors, All Rights Reserved.
