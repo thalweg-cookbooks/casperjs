58b6465e9428a99994b9997c2b012cd721f38faa
Nick Lopez <nlopez@amplify.com>
2015-06-22 19:00:23 -0400

    do terrible path hardcoding for now

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'nlopez@gmail.com'
 license 'all_rights'
 description 'Installs/Configures CasperJS'
 long_description 'Installs/Configures CasperJS'
-version '0.1.1'
+version '0.1.2'
 
 depends 'phantomjs'
 depends 'ark'
